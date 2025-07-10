92ead821d4dbaf275938516d4b44066cd32f375d
HARSH SAHU <127614860+Harshsahu11@users.noreply.github.com>
2025-07-10 11:36:30 +0530

    Simple Intrest

---
+++ SimpleIntrest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SimpleIntrest {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("Enter Principle Amount");
+        float P=Sc.nextFloat();
+        System.out.println("Enter Rate of Intrest");
+        float R=Sc.nextFloat();
+        System.out.println("Enter Time ");
+        float T=Sc.nextFloat();
+
+        float SI= (P*R*T)/100;
+        System.out.println("Simple Intrest : "+SI);
+    }
+    
+}
