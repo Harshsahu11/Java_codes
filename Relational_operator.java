296af79be7b11dd910eff24fb0cf7226b79cecb8
HARSH SAHU <127614860+Harshsahu11@users.noreply.github.com>
2025-07-10 11:59:21 +0530

    Relational Operator

---
+++ Relational_operator.java
@@ -0,0 +1,13 @@
+public class Relational_operator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        System.out.println(a==b); //Equals to
+        System.out.println(a!=b); //Not Equals to 
+        System.out.println(a<b);  //Less than
+        System.out.println(a>b);  //Greater than
+        System.out.println(a<=b); //less than equal to
+        System.out.println(a>=b); //greater than equal to
+    }
+}
