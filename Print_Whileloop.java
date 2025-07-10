3b5b63a387597aa2d0d00d70992f1718fc5c60cf
HARSH SAHU <127614860+Harshsahu11@users.noreply.github.com>
2025-07-10 14:12:31 +0530

    While Loop

---
+++ Print_Whileloop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Print_Whileloop {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("Enter Number");
+        int  n=Sc.nextInt();
+        int i=1;
+        while(i<=n){
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
