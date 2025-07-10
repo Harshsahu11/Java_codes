686ba2ff51c30a57b0d0bca876e05461396a0c20
HARSH SAHU <127614860+Harshsahu11@users.noreply.github.com>
2025-07-10 12:13:35 +0530

    Assignment Operator

---
+++ Assigment_Operator.java
@@ -0,0 +1,16 @@
+public class Assigment_Operator {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println("a= :"+a);
+        a+=5;
+        System.out.println("a+= :"+a);
+        a-=5;
+        System.out.println("a-= :"+a);
+        a*=5;
+        System.out.println("a*= :"+a);
+        a/=5;
+        System.out.println("a/= :"+a);
+        a%=5;
+        System.out.println("a%= :"+a);
+    }
+}
