5b0ba5cb4ee9dcd2c7eefeb889b39203b6ce5b9e
HARSH SAHU <127614860+Harshsahu11@users.noreply.github.com>
2025-07-11 11:50:30 +0530

    Array Sum

---
+++ Array/Sum.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Sum {
+    public static void main(String[] args) {
+        int arr[]={1,3,4};
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        System.out.println("Sum = "+sum);
+    }
+    
+}
